6f3521f6a8b3b055c483b06a3d0dfcd53199f1cb
taoyongming <taoyongming321@sina.com>
2019-12-01 22:14:04 +0800

    提交
    提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,10 +4,28 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    left: false ,
+    right: false,
+    activeIndex: 0
+  },
+  changeswiper: function(e) {
+    var index = e.detail.current;
+    if(index > this.data.activeIndex) {
+      this.setData({
+        left: true
+      })
+    } else if(index < this.data.activeIndex) {
+      this.setData({
+        right: true
+      })
+    }
+    setTimeout(() => {
+      this.setData({
+        activeIndex: index,
+        left:false,
+        right:false
+      })
+    }, 1000);
   },
   //事件处理函数
   bindViewTap: function() {
@@ -16,32 +34,15 @@ Page({
     })
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    // wx.request({
+    //   url: "https://www.easy-mock.com/mock/5a2b3948158e7b700327e702/getSwiList/swiList",
+    //   success: (res) => {
+    //     this.setData({
+    //       // console.log(res);
+    //       swiList: res.data.data.swiList
+    //     })
+    //   }
+    // })
   },
   getUserInfo: function(e) {
     console.log(e)
